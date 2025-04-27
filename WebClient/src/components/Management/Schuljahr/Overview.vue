323936db945ddbc7e408604e3961ee5de18578e7
Richard Schlossarek <richard-schlossarek@gmx.de>
2025-04-27 19:34:32 +0200

    Start restructure management to include multiple tabs

---
+++ WebClient/src/components/Management/Schuljahr/Overview.vue
@@ -0,0 +1,50 @@
+﻿<script setup>
+
+import {useSettings} from "@/stores/useSettings.js";
+import {Column, DataTable} from "primevue";
+
+const settings = useSettings();
+
+async function setup() {
+  await settings.updateSchuljahr();
+}
+
+setup();
+</script>
+
+<template>
+  <h2>Schuljahr</h2>
+  <DataTable :value="settings.schuljahr">
+    <Column header="Datum">
+      <template #body="{data}">{{
+          new Date(data.datum).toLocaleDateString('de-DE', {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric"
+          })
+        }}
+      </template>
+    </Column>
+    <Column header="Tag">
+      <template #body="{data}">{{
+          new Date(data.datum).toLocaleDateString('de-DE', {
+            weekday: "long",
+          })
+        }}
+      </template>
+    </Column>
+    <Column field="wochentyp" header="Wochentyp"/>
+    <Column header="Blöcke">
+      <template #body="{data}">
+        {{ data.blocks.sort().join(', ') }}
+      </template>
+    </Column>
+    <template #empty>
+      Keine Schultage angelegt.
+    </template>
+  </DataTable>
+</template>
+
+<style scoped>
+
+</style>
--- WebClient/src/views/Management/OtiaOverview.vue
+++ WebClient/src/views/Management/OtiaOverview.vue
@@ -1,25 +1,16 @@
 ﻿<script setup>
 import {useUser} from "@/stores/useUser.js";
-import {useSettings} from "@/stores/useSettings.js";
-import {Button, Column, DataTable, Dialog, Skeleton, useConfirm, useToast} from "primevue";
-import {ref} from "vue";
-import {mande} from "mande";
-import AfraKategorieTag from "@/components/Otium/Shared/AfraKategorieTag.vue";
-import {findPath} from "@/helpers/tree.js";
-import SimpleBreadcrumb from "@/components/SimpleBreadcrumb.vue";
-import {RouterLink} from "vue-router";
-import CreateOtiumForm from "@/components/Form/CreateOtiumForm.vue";
+import OtiumOverview from "@/components/Otium/Management/Overview.vue";
+import SchuljahrOverview from "@/components/Management/Schuljahr/Overview.vue";
+import Tabs from 'primevue/tabs';
+import TabList from 'primevue/tablist';
+import Tab from 'primevue/tab';
+import TabPanels from 'primevue/tabpanels';
+import TabPanel from 'primevue/tabpanel';
 import NavBreadcrumb from "@/components/NavBreadcrumb.vue";
+import {ref} from "vue";
 
 const user = useUser();
-const settings = useSettings();
-const toast = useToast();
-const confirm = useConfirm();
-
-const loading = ref(true);
-const createDialogOpen = ref(false);
-
-const otia = ref([]);
 
 const navItems = ref([
   {
@@ -30,147 +21,29 @@ const navItems = ref([
   }
 ])
 
-async function getOtia() {
-  const getter = mande('/api/otium/management/otium');
-  otia.value = await getter.get();
-}
-
-async function deleteOtium(id) {
-  const api = mande('/api/otium/management/otium/' + id);
-  try {
-    await api.delete();
-  } catch {
-    toast.add({
-      severity: "error",
-      summary: "Fehler",
-      detail: "Ein unerwarteter Fehler ist beim Löschen des Otiums aufgetreten"
-    });
-  } finally {
-    await getOtia();
-  }
-}
-
-async function createOtium(data) {
-  const api = mande('/api/otium/management/otium');
-  try {
-    await api.post(data)
-  } catch {
-    toast.add({
-      severity: "error",
-      summary: "Fehler",
-      detail: "Ein unerwarteter Fehler ist beim Erstellen des Otiums aufgetreten"
-    });
-  } finally {
-    createDialogOpen.value = false;
-    await getOtia();
-  }
-}
-
-function openCreateDialog() {
-  createDialogOpen.value = true;
-}
-
-const openConfirmDialog = (event, callback, message) => {
-  confirm.require({
-    target: event.currentTarget,
-    message: message,
-    icon: 'pi pi-exclamation-triangle',
-    acceptProps: {
-      label: 'Ja',
-      severity: 'danger'
-    },
-    rejectProps: {
-      label: 'Nein',
-      severity: 'secondary'
-    },
-    accept: callback
-  });
-}
-
-const confirmDelete = (event, id) => {
-  const onConfirm = () => deleteOtium(id);
-  openConfirmDialog(event, onConfirm, "Otium löschen?")
-}
-
-async function setup() {
-  try {
-    await settings.updateKategorien();
-    await getOtia();
-    loading.value = false;
-  } catch {
-    toast.add({
-      severity: "error",
-      summary: "Fehler",
-      detail: "Ein unerwarteter Fehler ist beim Laden der Daten aufgetreten"
-    });
-    await user.update();
-  }
-}
-
-setup();
 </script>
 
 <template>
   <template v-if="user.user.rolle !== 'Tutor'">
     <h1>Sie sind nicht Autorisiert, diese Seite zu nutzen.</h1>
   </template>
-  <template v-else-if="!loading">
-    <NavBreadcrumb :items="navItems"/>
-    <h1>Alle Otia</h1>
-    <p>Klicken sie auf ein Otium, um Details zu sehen oder es zu Bearbeiten.</p>
-    <DataTable :value="otia">
-      <Column header="Bezeichnung">
-        <template #body="{data}">
-          <Button :as="RouterLink" :label="data.bezeichnung"
-                  :to="{name: 'Verwaltung-Otium', params: {otiumId: data.id}}" variant="text"/>
-        </template>
-      </Column>
-      <Column header="Kategorie">
-        <template #body="{data}">
-          <SimpleBreadcrumb :model="findPath(settings.kategorien, data.kategorie)" wrap>
-            <template #item="{item}">
-              <AfraKategorieTag :value="item" minimal/>
-            </template>
-          </SimpleBreadcrumb>
-        </template>
-      </Column>
-      <Column class="text-right" header="Termine">
-        <template #body="{data}">
-          {{ data.termine.length }}
-        </template>
-      </Column>
-      <Column class="text-right afra-col-action">
-        <template #header>
-          <Button v-tooltip="'Neues Otium'" icon="pi pi-plus" @click="openCreateDialog"/>
-        </template>
-        <template #body="{data}">
-          <Button v-if="!data.termine || data.termine.length===0" v-tooltip.left="'Löschen'"
-                  icon="pi pi-times" severity="danger" variant="text"
-                  @click="(event) => confirmDelete(event, data.id)"/>
-          <Button v-else v-tooltip.left="'Nur Otia ohne Termine können gelöscht werden'" disabled
-                  icon="pi pi-times"
-                  severity="secondary" variant="text"/>
-        </template>
-      </Column>
-    </DataTable>
-    <Dialog v-model:visible="createDialogOpen" :style="{width: '35rem'}" header="Otium hinzufügen"
-            modal>
-      <CreateOtiumForm @submit="createOtium"/>
-    </Dialog>
-  </template>
   <template v-else>
-    <Skeleton class="mb-6" height="3rem"/>
-    <Skeleton class="mb-4"/>
-    <DataTable :value="new Array(10)">
-      <Column v-for="_ in new Array(3)">
-        <template #body>
-          <Skeleton/>
-        </template>
-        <template #header>
-          <Skeleton height="1.5em"/>
-        </template>
-      </Column>
-    </DataTable>
+
+    <NavBreadcrumb :items="navItems"/>
+    <Tabs value="0">
+      <TabList>
+        <Tab value="0">Otium</Tab>
+        <Tab value="1">Schultage</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel value="0">
+          <OtiumOverview/>
+        </TabPanel>
+        <TabPanel value="1">
+          <SchuljahrOverview/>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   </template>
 </template>
 
