1461c874489c7e8cc8cfe3a6747311256c3ea432
Richard Schlossarek <r.schlossarek@verein-der-altafraner.de>
2025-06-12 11:53:15 +0200

    Initial selector form

---
+++ WebClient/src/Profundum/components/EinwahlSelector.vue
@@ -0,0 +1,38 @@
+﻿<script lang="ts" setup>
+import FloatLabel from "primevue/floatlabel";
+import {Select} from "primevue";
+
+const props = defineProps({
+  options: {
+    type: Array,
+    required: true
+  },
+  label: String,
+  disabled: Boolean,
+  preSelected: {
+    type: Object,
+    required: false
+  }
+})
+
+const model = defineModel({
+  default: null
+});
+</script>
+
+<template>
+  <div class="w-full">
+    <FloatLabel variant="on">
+      <Select v-if="!disabled" id="erstwunsch" v-model="model" :options="props.options"
+              fluid
+              name="erstwunsch" option-label="label" option-value="value" show-clear/>
+      <Select v-else id="erstwunsch" v-model="model" :options="[preSelected]" disabled
+              fluid name="erstwunsch" option-label="label" option-value="value" show-clear/>
+      <label for="erstwunsch">{{ label }}</label>
+    </FloatLabel>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ WebClient/src/Profundum/router/routes.js
@@ -0,0 +1,7 @@
+﻿export const routes = [
+  {
+    path: "/profundum/katalog",
+    name: "Profundum-Einwahl",
+    component: () => import('@/Profundum/views/Einwahl.vue')
+  },
+];
