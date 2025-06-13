d6582b10188a6ba629e8f92e10df3c2dec167923
Richard Schlossarek <r.schlossarek@verein-der-altafraner.de>
2025-06-13 09:11:38 +0200

    Extract testdata

---
+++ WebClient/src/Profundum/components/testdata.js
@@ -0,0 +1,46 @@
+﻿export const testdata = [
+  {
+    label: "Q1 - Dienstag",
+    id: "q1-di",
+    options: [
+      {label: "Option 1", value: "option1di1"},
+      {label: "Option 2", value: "option1di2"},
+      {label: "Option 3", value: "option1di3"},
+      {label: "Option 4", value: "option1di4", alsoIncludes: ["q1-do"]},
+      {label: "Option 5", value: "option1di5"}
+    ]
+  },
+  {
+    label: "Q1 - Donnerstag",
+    id: "q1-do",
+    options: [
+      {label: "Option 1", value: "option1do1"},
+      {label: "Option 2", value: "option1do2"},
+      {label: "Option 3", value: "option1do3"},
+      {label: "Option 4", value: "option1do4"},
+      {label: "Option 5", value: "option1do5"}
+    ]
+  },
+  {
+    label: "Q2 - Dienstag",
+    id: "q2-di",
+    options: [
+      {label: "Option 1", value: "option2di1"},
+      {label: "Option 2", value: "option2di2"},
+      {label: "Option 3", value: "option2di3"},
+      {label: "Option 4", value: "option2di4"},
+      {label: "Option 5", value: "option2di5"}
+    ]
+  },
+  {
+    label: "Q2 - Donnerstag",
+    id: "q2-do",
+    options: [
+      {label: "Option 1", value: "option2do1"},
+      {label: "Option 2", value: "option2do2"},
+      {label: "Option 3", value: "option2do3"},
+      {label: "Option 4", value: "option2do4"},
+      {label: "Option 5", value: "option2do5"}
+    ]
+  }
+]
