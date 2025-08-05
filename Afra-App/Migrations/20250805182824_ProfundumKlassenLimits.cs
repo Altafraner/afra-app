4fc52f46eac85ae6b962ae190228d1c38b39d8cf
Johann Höpfner <mail@johann-hoepfner.de>
2025-08-05 22:52:32 +0200

    Profundum: Klassenbeschränkung

---
+++ Afra-App/Migrations/20250805182824_ProfundumKlassenLimits.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Afra_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProfundumKlassenLimits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "maxKlasse",
+                table: "Profunda",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "minKlasse",
+                table: "Profunda",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "maxKlasse",
+                table: "Profunda");
+
+            migrationBuilder.DropColumn(
+                name: "minKlasse",
+                table: "Profunda");
+        }
+    }
+}
