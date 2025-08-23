01cb8ddc019a9a2be12b8f3f5fce894dc418d115
Johann Höpfner <mail@johann-hoepfner.de>
2025-08-23 18:08:24 +0200

    Styling fixes

--- WebClient/src/Otium/components/Supervision/AfraOtiumSupervisionView.vue
+++ WebClient/src/Otium/components/Supervision/AfraOtiumSupervisionView.vue
@@ -112,14 +112,14 @@ function initMove(student, terminId) {
           <span class="flex-1">
             {{ room.ort }} - {{ room.otium }}
           </span>
-          <span class="text-right pr-4">
+          <span class="inline-flex gap-3">
             {{ room.einschreibungen.length }} Schüler:innen
+            <Button :label="room.sindAnwesenheitenErfasst ? 'Fertig' : 'Ausstehend'"
+                    :severity="room.sindAnwesenheitenErfasst ? 'success' : 'danger'"
+                    class="confirm-button"
+                    size="small"
+                    @click="(evt) => updateStatusCallback(evt, room.terminId, !room.sindAnwesenheitenErfasst)"/>
           </span>
-          <Button :label="room.sindAnwesenheitenErfasst ? 'Fertig' : 'Ausstehend'"
-                  :severity="room.sindAnwesenheitenErfasst ? 'success' : 'danger'"
-                  class="confirm-button"
-                  size="small"
-                  @click="(evt) => updateStatusCallback(evt, room.terminId, !room.sindAnwesenheitenErfasst)"/>
         </div>
       </accordion-header>
       <accordion-content>
