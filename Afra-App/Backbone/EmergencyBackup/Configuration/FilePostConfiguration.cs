a5d8604f13ccd4eccce8116090ddbddc9e16ca15
Richard Schlossarek <r.schlossarek@verein-der-altafraner.de>
2025-08-24 12:44:14 +0200

    Add emergency backup service

---
+++ Afra-App/Backbone/EmergencyBackup/Configuration/FilePostConfiguration.cs
@@ -0,0 +1,27 @@
+﻿namespace Afra_App.Backbone.EmergencyBackup.Configuration;
+
+/// <summary>
+/// Configuration settings for posting files to an external endpoint for emergency backup purposes.
+/// </summary>
+public class FilePostConfiguration
+{
+    /// <summary>
+    /// The name of the HttpClient to be used for emergency backup operations.
+    /// </summary>
+    public const string HttpClientName = "EmergencyBackup";
+
+    /// <summary>
+    /// The base URL of the endpoint where HTML content will be posted for emergency backup.
+    /// </summary>
+    public required string BaseUri { get; set; }
+
+    /// <summary>
+    /// The username for basic authentication when posting to the endpoint. Optional.
+    /// </summary>
+    public string? Username { get; set; }
+
+    /// <summary>
+    /// The password for basic authentication when posting to the endpoint. Optional.
+    /// </summary>
+    public string? Password { get; set; }
+}
