95fb001ce98519bacbbc8c07050ced57c049b6ca
Richard Schlossarek <r.schlossarek@verein-der-altafraner.de>
2025-08-24 12:44:14 +0200

    Add emergency backup service

---
+++ Afra-App/Backbone/EmergencyBackup/Services/Contracts/IEmergencyBackupService.cs
@@ -0,0 +1,14 @@
+﻿namespace Afra_App.Backbone.EmergencyBackup.Services.Contracts;
+
+/// <summary>
+///     Specifies methods for saving HTML content as part of an emergency backup service.
+/// </summary>
+public interface IEmergencyBackupService
+{
+    /// <summary>
+    /// Saves the provided HTML content with the specified name to the backup service.
+    /// </summary>
+    /// <param name="name">The name of the file to save</param>
+    /// <param name="content">The files contents</param>
+    public Task SaveHtmlAsync(string name, string content);
+}
---
+++ Afra-App/Backbone/EmergencyBackup/Services/Implementations/FilePostEmergencyBackup.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using Afra_App.Backbone.EmergencyBackup.Configuration;
+using Afra_App.Backbone.EmergencyBackup.Services.Contracts;
+
+namespace Afra_App.Backbone.EmergencyBackup.Services.Implementations;
+
+/// <summary>
+///     An implementation of <see cref="IEmergencyBackupService" /> that saves HTML content via HTTP POST requests to a specified endpoint.
+/// </summary>
+public class FilePostEmergencyBackup : IEmergencyBackupService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly ILogger<FilePostEmergencyBackup> _logger;
+
+    ///
+    public FilePostEmergencyBackup(IHttpClientFactory httpClientFactory, ILogger<FilePostEmergencyBackup> logger)
+    {
+        _httpClientFactory = httpClientFactory;
+        _logger = logger;
+    }
+
+    /// <inheritdoc />
+    public async Task SaveHtmlAsync(string name, string content)
+    {
+        _logger.LogInformation("Saving {Name}", name);
+        var client = _httpClientFactory.CreateClient(FilePostConfiguration.HttpClientName);
+
+        // Convert the HTML content to a byte array
+        var byteArray = Encoding.UTF8.GetBytes(content);
+        var byteContent = new ByteArrayContent(byteArray);
+        byteContent.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+
+        // Sanitize the file name by replacing non-alphanumeric characters with hyphens
+        var fileName = string.Concat(name.Select(c => char.IsAsciiLetterOrDigit(c) ? c : '-'), ".html");
+
+        var formData = new MultipartFormDataContent();
+        formData.Add(byteContent, "file", fileName);
+
+        try
+        {
+            var response = await client.PostAsync("/upload", formData);
+            response.EnsureSuccessStatusCode();
+            _logger.LogInformation("Successfully posted emergency backup {Name}", name);
+        }
+        catch (HttpRequestException e)
+        {
+            _logger.LogError("Error posting emergency backup {Name}: {Message}", name, e.Message);
+            throw;
+        }
+    }
+}
