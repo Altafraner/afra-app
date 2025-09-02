105abb9fcad03b03bfea9b2cf8df7f2204d0bbaf
Richard Schlossarek <r.schlossarek@verein-der-altafraner.de>
2025-09-02 19:40:22 +0200

    Add rules to DI container

---
+++ Afra-App/Otium/Extensions/AddRulesExtension.cs
@@ -0,0 +1,27 @@
+using Afra_App.Otium.Domain.Contracts.Rules;
+using Afra_App.Otium.Services.Rules;
+
+namespace Afra_App.Otium.Extensions;
+
+/// <summary>
+/// Contains extension methods to add built-in rules to the service collection.
+/// </summary>
+public static class AddRulesExtension
+{
+    /// <summary>
+    /// Adds all built-in rules to the service collection.
+    /// </summary>
+    public static void AddRules(this IServiceCollection services)
+    {
+        services.AddScoped<IBlockRule, AlwaysAttendedRule>();
+        services.AddScoped<IBlockRule, MustEnrollRule>();
+        services.AddScoped<IBlockRule, ParallelEnrollmentRule>();
+
+        services.AddScoped<IIndependentRule, EnrollmentTimeframeRule>();
+        services.AddScoped<IIndependentRule, MaxEnrollmentsRule>();
+        services.AddScoped<IIndependentRule, MustBeStudentRule>();
+        services.AddScoped<IIndependentRule, NotCancelledRule>();
+
+        services.AddScoped<IWeekRule, RequiredKategorienRule>();
+    }
+}
