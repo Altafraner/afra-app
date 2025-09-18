fb79513b2376f313c6e0e97954135dd551551bca
BuildTools <unconfigured@null.spigotmc.org>
2025-09-18 22:21:34 +0200

    Backend/Database Implementation - Working on frontend

---
+++ Afra-App/Profundum/Domain/Models/ProfundumBewertung.cs
@@ -0,0 +1,30 @@
+using Afra_App.Profundum.Domain.Models;
+using Afra_App.User.Domain.Models;
+
+/// <summary>
+///  A db record representing a Profundum Bewertung.
+/// </summary>
+public class ProfundumBewertung
+{
+    /// <summary>
+    ///  The unique identifier for the Bewertung
+    /// </summary>
+    public Guid Id { get; set; }
+    /// <summary>
+    ///   The unique identifier for the Bewertung
+    /// </summary>
+    public required ProfundumBewertungKriterium Kriterium { get; set; }
+    /// <summary> 
+    ///   The profundum instanz the Bewertung is for
+    /// </summary>
+    public required ProfundumInstanz Instanz { get; set; }
+    /// <summary>
+    ///   The person that received the Bewertung
+    /// </summary>
+    public required Person BetroffenePerson { get; set; }
+    /// <summary>
+    ///   The grade given for the Kriterium
+    /// </summary>
+    public int Grad { get; set; }
+}
+
