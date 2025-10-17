2667bfee66d9351d2a5271cbed47b21aa328a0b4
Richard Schlossarek <r.schlossarek@verein-der-altafraner.de>
2025-10-17 15:54:00 +0200

    Unbrick EF Core Design tools

---
+++ Backend/Altafraner.AfraApp/Database/AfraAppContextDesignTimeFactory.cs
@@ -0,0 +1,27 @@
+﻿using Altafraner.Backbone;
+using Microsoft.EntityFrameworkCore.Design;
+
+// ReSharper disable UnusedType.Global
+
+namespace Altafraner.AfraApp;
+
+/// <summary>
+///     This is a factory used for design time creation of the AfraAppContext. This will be used by the ef core design
+///     tools.
+/// </summary>
+public class AfraAppContextDesignTimeFactory : IDesignTimeDbContextFactory<AfraAppContext>
+{
+    /// <inheritdoc />
+    public AfraAppContext CreateDbContext(string[] args)
+    {
+        // We'll create the service here exactly the way we would in the normal application.
+        var builder = WebApplication.CreateSlimBuilder(new WebApplicationOptions
+        {
+            Args = args
+        });
+        builder.UseAltafranerBackbone(backbone => backbone.AddModule<DatabaseModule>());
+        var app = builder.Build();
+        var scope = app.Services.CreateScope();
+        return scope.ServiceProvider.GetRequiredService<AfraAppContext>();
+    }
+}
