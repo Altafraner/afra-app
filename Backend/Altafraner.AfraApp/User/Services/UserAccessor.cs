0af7f10c7fedc2967221303763e4c1df78781046
Richard Schlossarek <r.schlossarek@verein-der-altafraner.de>
2025-10-17 15:10:29 +0200

    Add GetUserId method to UserAccessor.cs

--- Backend/Altafraner.AfraApp/User/Services/UserAccessor.cs
+++ Backend/Altafraner.AfraApp/User/Services/UserAccessor.cs
@@ -37,32 +37,62 @@ public class UserAccessor
     /// </exception>
     public async Task<Person> GetUserAsync()
     {
-        var httpContext = _httpContextAccessor.HttpContext;
-        if (httpContext is null)
-            throw new InvalidOperationException("The HttpContext is not available!");
+        var httpContext = GetHttpContextOrThrow();
 
+        var cachedUser = GetUserFromCache(httpContext);
+        if (cachedUser != null) return cachedUser;
+
+        var user = await _dbContext.Personen.FindAsync(GetUserIdOrThrow(httpContext));
+
+        if (user is null)
+        {
+            using var scope = _serviceScopeFactory.CreateScope();
+            var authenticationLifetimeService =
+                scope.ServiceProvider.GetRequiredService<IAuthenticationLifetimeService>();
+            await authenticationLifetimeService.SignOutAsync();
+            throw new KeyNotFoundException("The specified User does not exist");
+        }
+
+        httpContext.Items.Add(UserPersonObjectCacheKey, user);
+        return user;
+    }
+
+    /// <summary>
+    ///     Gets the currently logged-in users id
+    /// </summary>
+    /// <exception cref="InvalidOperationException">
+    ///     There is no <see cref="HttpContext" /> or the user is not signed in.
+    /// </exception>
+    /// <exception cref="KeyNotFoundException">
+    ///     The user identified by the <see cref="ClaimsPrincipal" /> does not exist in the database.
+    /// </exception>
+    public Guid GetUserId()
+    {
+        var httpContext = GetHttpContextOrThrow();
+        return GetUserFromCache(httpContext)?.Id ?? GetUserIdOrThrow(httpContext);
+    }
+
+    private HttpContext GetHttpContextOrThrow()
+    {
+        return _httpContextAccessor.HttpContext ??
+               throw new InvalidOperationException("The HttpContext is not available!");
+    }
+
+    private Person? GetUserFromCache(HttpContext httpContext)
+    {
         if (httpContext.Items.TryGetValue(UserPersonObjectCacheKey, out var cachedUserObject) &&
             cachedUserObject is Person cachedUser)
             return cachedUser;
+        return null;
+    }
 
+    private Guid GetUserIdOrThrow(HttpContext httpContext)
+    {
         if (!httpContext.User.Identity?.IsAuthenticated ?? true)
             throw new InvalidOperationException("The user is not logged in!");
 
-        if (!httpContext.User.HasClaim(claim => claim.Type == AfraAppClaimTypes.Id))
-            throw new InvalidOperationException($"The user does not have a {AfraAppClaimTypes.Id} claim");
-
-        var user = await _dbContext.Personen.FindAsync(new Guid(httpContext.User.Claims
-            .First(claim => claim.Type == AfraAppClaimTypes.Id).Value));
-
-        if (user is not null)
-        {
-            httpContext.Items.Add(UserPersonObjectCacheKey, user);
-            return user;
-        }
-
-        using var scope = _serviceScopeFactory.CreateScope();
-        var authenticationLifetimeService = scope.ServiceProvider.GetRequiredService<IAuthenticationLifetimeService>();
-        await authenticationLifetimeService.SignOutAsync();
-        throw new KeyNotFoundException("The specified User does not exist");
+        return !httpContext.User.HasClaim(claim => claim.Type == AfraAppClaimTypes.Id)
+            ? throw new InvalidOperationException($"The user does not have a {AfraAppClaimTypes.Id} claim")
+            : new Guid(httpContext.User.Claims.First(claim => claim.Type == AfraAppClaimTypes.Id).Value);
     }
 }
