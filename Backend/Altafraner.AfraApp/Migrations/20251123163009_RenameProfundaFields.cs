d9ff7a43262952c53694dc96c0a809cb8be1dce1
Richard Schlossarek <r.schlossarek@verein-der-altafraner.de>
2025-11-23 17:30:52 +0100

    Add migration to end code style fixes

---
+++ Backend/Altafraner.AfraApp/Migrations/20251123163009_RenameProfundaFields.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altafraner.AfraApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameProfundaFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "minKlasse",
+                table: "Profunda",
+                newName: "MinKlasse");
+
+            migrationBuilder.RenameColumn(
+                name: "maxKlasse",
+                table: "Profunda",
+                newName: "MaxKlasse");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MinKlasse",
+                table: "Profunda",
+                newName: "minKlasse");
+
+            migrationBuilder.RenameColumn(
+                name: "MaxKlasse",
+                table: "Profunda",
+                newName: "maxKlasse");
+        }
+    }
+}
