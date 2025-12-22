89e61820a00d6e20346a5e1de0dfc6bc34bfbbf8
Richard Schlossarek <r.schlossarek@verein-der-altafraner.de>
2025-12-22 16:05:24 +0100

    feedback api client

---
+++ WebClient/src/Profundum/composables/verwaltung.ts
@@ -0,0 +1,39 @@
+﻿import { useToast } from 'primevue';
+import { mande, type MandeError } from 'mande';
+import type { QuartalEnrollmentOverview } from '@/Profundum/models/feedback';
+import type { ProfundumKategorie } from '@/Profundum/models/verwaltung';
+
+export const useManagement = () => {
+    const toast = useToast();
+    const api = mande('/api/profundum/management');
+
+    async function getAllQuartaleWithEnrollments(): Promise<
+        QuartalEnrollmentOverview[] | null
+    > {
+        try {
+            return await api.get('/management/belegung');
+        } catch (e) {
+            const mandeError: MandeError = e;
+            toast.add({
+                summary: 'Es ist ein Fehler aufgetreten',
+                detail: `Die Profunda konnten nicht geladen werden. Code ${mandeError.response.status}, ${mandeError.message}`,
+            });
+            return null;
+        }
+    }
+
+    async function getKategorien(): Promise<ProfundumKategorie[]> {
+        try {
+            return await api.get('/kategorie');
+        } catch (e) {
+            const mandeError: MandeError = e;
+            toast.add({
+                summary: 'Es ist ein Fehler aufgetreten',
+                detail: `Die verfügbaren Kategorien der Profunda konnten nicht geladen werden. Code ${mandeError.response.status}, ${mandeError.message}`,
+            });
+            return null;
+        }
+    }
+
+    return { getAllQuartaleWithEnrollments, getKategorien };
+};
