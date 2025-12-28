efeef9c974626a590733e5b05f0f597c2b2bed8a
Richard Schlossarek <r.schlossarek@verein-der-altafraner.de>
2025-12-28 17:50:10 +0100

    Typst template source generator
    There is currently a dependency conflict when using the newest version of Microsoft.CodeAnalysis.Common. Some EF Core package depends on the old version. we should look at that again in a few weeks when they've hopefully released a new version.

--- Backend/Altafraner.AfraApp/Profundum/Services/ProfundumManagementService.cs
+++ Backend/Altafraner.AfraApp/Profundum/Services/ProfundumManagementService.cs
@@ -391,69 +391,7 @@ internal class ProfundumManagementService
             .Where(e => e.ProfundumInstanz.Id == p.Id)
             .Select(e => e.BetroffenePerson);
 
-        var src = $$"""
-            #import "@preview/cmarker:0.1.8"
-            #import "@preview/mitex:0.2.6": mitex
-
-            #let bezeichnung = sys.inputs.bezeichnung
-            #let beschreibung = sys.inputs.beschreibung
-            #let slots = json(bytes(sys.inputs.slots))
-            #let verantwortliche = json(bytes(sys.inputs.verantwortliche))
-            #let teilnehmer = json(bytes(sys.inputs.teilnehmer))
-
-            #align(right, image("logo.png", width: 50mm))
-
-            #let accent-color = rgb("#0069B4")
-            #let accent-font = "TheSerif"
-
-            #show heading.where(level: 1): set text(size: 22pt, font: accent-font, weight: 500, fill: accent-color)
-            #show heading.where(level: 2): set text(size: 17pt, font: accent-font, weight: 500, fill: accent-color)
-            #show heading.where(level: 3): set text(size: 14pt, font: accent-font, weight: 500, fill: accent-color)
-            #show heading.where(level: 4): set text(size: 12pt, font: accent-font, weight: 500, fill: accent-color)
-
-            = Profundum: #bezeichnung
-
-            #let weekdays = (
-              "Sonntag",
-              "Montag",
-              "Dienstag",
-              "Mittwoch",
-              "Donnerstag",
-              "Freitag",
-              "Samstag",
-            )
-
-            #table(columns: 3,stroke:none,
-                ..for s in slots {
-                    ([#s.Jahr/#{calc.rem-euclid(s.Jahr + 1, 100)}], [Q#s.Quartal], weekdays.at(s.Wochentag))
-                }
-            )
-
-            == Inhalt
-
-
-            #cmarker.render(math:mitex,h1-level:3,
-                beschreibung
-            )
-
-            == Verantwortliche
-
-            #table(columns: 3,stroke:none,
-                ..for v in verantwortliche {
-                    (v.Nachname, v.Vorname, v.Email)
-                }
-            )
-
-            == Teilnehmer
-
-            #table(columns: 3,
-                ..for v in teilnehmer {
-                    (v.Gruppe, v.Nachname, v.Vorname,)
-                }
-            )
-            """;
-
-
+        const string src = Typst.Templates.Profundum.Instanz;
         var typst = new TypstCompilerWrapper(src, null, _typstConfig.Value.TypstResourcePath);
 
         typst.SetSysInputs(new Dictionary<string, string> {
---
+++ Backend/Altafraner.Typst.Generator/TypstTemplateGenerator.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Altafraner.Typst.Generator;
+
+/// <summary>
+///   A Source generator for converting '.typ' files to static classes
+/// </summary>
+[Generator]
+public class TypstTemplateGenerator : IIncrementalGenerator
+{
+    /// <inheritdoc />
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var textFiles = context.AdditionalTextsProvider.Where(static file => file.Path.EndsWith(".typ"));
+        var namesAndContents = textFiles.Select((text, cancellationToken) => (
+            name: Path.GetFileNameWithoutExtension(text.Path),
+            Directory: Path.GetFileName(Path.GetDirectoryName(text.Path)),
+            content: text.GetText(cancellationToken)!.ToString()));
+        context.RegisterSourceOutput(namesAndContents,
+            (spc, source) =>
+            {
+                var (name, directory, content) = source;
+                var safeContent = SymbolDisplay.FormatLiteral(content, true);
+                spc.AddSource(directory + "__" + name,
+                    $$"""
+                      #pragma warning disable CS1591
+                      namespace Altafraner.Typst.Templates;
+
+                      public static partial class {{directory}}
+                      {
+                          public const string {{name}} = {{safeContent}};
+                      }
+                      #pragma warning restore CS1591
+                      """);
+            });
+    }
+}
