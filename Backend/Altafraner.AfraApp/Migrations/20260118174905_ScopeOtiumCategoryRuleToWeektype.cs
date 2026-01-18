72ea9ab6330b1e3ce997f2f7f13c40c6f10eb873
Richard Schlossarek <r.schlossarek@verein-der-altafraner.de>
2026-01-18 19:21:09 +0100

    Shit, forgot to push migration

---
+++ Backend/Altafraner.AfraApp/Migrations/20260118174905_ScopeOtiumCategoryRuleToWeektype.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Altafraner.AfraApp.Schuljahr.Domain.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altafraner.AfraApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScopeOtiumCategoryRuleToWeektype : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Required",
+                table: "OtiaKategorien");
+
+            migrationBuilder.AddColumn<List<Wochentyp>>(
+                name: "RequiredIn",
+                table: "OtiaKategorien",
+                type: "wochentyp[]",
+                defaultValue: new List<Wochentyp>(),
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RequiredIn",
+                table: "OtiaKategorien");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Required",
+                table: "OtiaKategorien",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
