343b6aca7aa1785b84dc11deefea75dda4ca97bd
Richard Schlossarek <r.schlossarek@verein-der-altafraner.de>
2026-01-21 09:49:36 +0100

    Add support for sanitized inline markdown rendering

--- WebClient/src/composables/markdown.ts
+++ WebClient/src/composables/markdown.ts
@@ -1,7 +1,9 @@
 ﻿import { marked } from 'marked';
 import DOMPurify from 'dompurify';
 
-export function convertMarkdownToHtml(markdown: string): string {
-    const markdownAsHtml = marked.parse(markdown, { async: false });
+export function convertMarkdownToHtml(markdown: string, inline: boolean = false): string {
+    const markdownAsHtml = inline
+        ? marked.parseInline(markdown, { async: false })
+        : marked.parse(markdown, { async: false });
     return DOMPurify.sanitize(markdownAsHtml);
 }
