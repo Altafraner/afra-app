5ac61170369b690eedfdfb4e52f7a893ff07a188
Johann Höpfner <mail@johann-hoepfner.de>
2026-02-20 18:42:22 +0100

    Show basic Calendar on Dashboard

--- WebClient/src/views/Home.vue
+++ WebClient/src/views/Home.vue
@@ -2,6 +2,9 @@
 import { useUser } from '@/stores/user';
 import { defineAsyncComponent } from 'vue';
 import { useOtiumStore } from '@/Otium/stores/otium.js';
+import FullCalendar from '@fullcalendar/vue3';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import iCalendarPlugin from '@fullcalendar/icalendar';
 
 const Student = defineAsyncComponent(() => import('@/Otium/views/Dashboard/Student.vue'));
 const Teacher = defineAsyncComponent(() => import('@/Otium/views/Dashboard/Teacher.vue'));
@@ -9,11 +12,27 @@ const Teacher = defineAsyncComponent(() => import('@/Otium/views/Dashboard/Teach
 const user = useUser();
 const otiumStore = useOtiumStore();
 await otiumStore.updateKategorien();
+
+const calendarOptions = {
+    plugins: [timeGridPlugin, iCalendarPlugin],
+    initialView: 'timeGridWeek',
+    slotMinTime: '12:00:00',
+    slotMaxTime: '21:00:00',
+    nowIndicator: true,
+    expandRows: true,
+    allDaySlot: false,
+    weekends: false,
+    events: {
+        url: '/api/calendar.ics',
+        format: 'ics',
+    },
+};
 </script>
 
 <template>
     <Student v-if="user.isStudent" />
     <Teacher v-if="user.isTeacher" />
+    <FullCalendar class="mt-10" :options="calendarOptions" />
 </template>
 
 <style scoped>
