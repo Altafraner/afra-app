b84f3c09576408dd06774c4dfc4d333ace94a52f
Richard Schlossarek <r.schlossarek@verein-der-altafraner.de>
2026-03-15 14:44:02 +0100

    Add student search function in supervision

--- WebClient/src/Otium/components/Supervision/OtiumSupervision.vue
+++ WebClient/src/Otium/components/Supervision/OtiumSupervision.vue
@@ -5,16 +5,19 @@ import {
     AccordionHeader,
     AccordionPanel,
     Button,
+    InputGroup,
+    InputGroupAddon,
     useDialog,
     useToast,
 } from 'primevue';
-import { computed, onUnmounted } from 'vue';
+import { computed, onUnmounted, shallowRef, watch } from 'vue';
 import MoveStudentForm from '@/Otium/components/Supervision/MoveStudentForm.vue';
 import OtiumEnrollmentTable from '@/Otium/components/Management/OtiumEnrollmentTable.vue';
 import { useAttendance } from '@/Otium/composables/attendanceHubClient.js';
 import { useRoute } from 'vue-router';
 import { isNowInInterval } from '@/helpers/time.js';
 import SelectStudentToMoveForm from '@/Otium/components/Supervision/SelectStudentToMoveForm.vue';
+import PersonSelector from '@/components/PersonSelector.vue';
 
 const props = defineProps({
     rooms: Array,
@@ -25,6 +28,9 @@ const props = defineProps({
 const toast = useToast();
 const dialog = useDialog();
 const route = props.useQueryBlock ? useRoute() : undefined;
+
+const filterPerson = shallowRef();
+const accordionValue = shallowRef(null);
 
 const useDataFromQuery = computed(
     () => props.useQueryBlock && route.query.blockId !== undefined,
@@ -109,18 +115,60 @@ function initMoveHere(terminId) {
         attendanceService.moveStudentNow(data.student, blockId.value, terminId);
     }
 }
+
+const filterActive = computed(() => filterPerson.value != undefined);
+
+const filteredAttendance = computed(() => {
+    if (!filterPerson.value) return attendance.value;
+    return attendance.value
+        .map((a) => {
+            const temp = Object.assign({}, a);
+            temp.einschreibungen = a.einschreibungen?.filter(
+                (e) => e.student.id === filterPerson.value,
+            );
+            return temp;
+        })
+        .filter((a) => a.einschreibungen?.length ?? 0 > 0);
+});
+
+watch(filteredAttendance, (newAttendance) => {
+    if (newAttendance.length === 1) accordionValue.value = newAttendance[0].terminId;
+});
 </script>
 
 <template>
-    <accordion lazy>
-        <accordion-panel v-for="room of attendance" :key="room.terminId" :value="room.terminId">
+    <InputGroup class="mb-6">
+        <PersonSelector
+            v-model="filterPerson"
+            :filter="(s) => s.rolle === 'Mittelstufe'"
+            hide-rolle
+        >
+            <template #label>Schüler:in suchen</template>
+        </PersonSelector>
+        <InputGroupAddon>
+            <Button
+                :disabled="filterPerson == undefined"
+                aria-label="Filter entfernen"
+                icon="pi pi-times"
+                severity="secondary"
+                variant="text"
+                @click="filterPerson = undefined"
+            />
+        </InputGroupAddon>
+    </InputGroup>
+    <accordion v-model:value="accordionValue" lazy>
+        <accordion-panel
+            v-for="room of filteredAttendance"
+            :key="room.terminId"
+            :value="room.terminId"
+        >
             <accordion-header>
                 <div
                     class="flex justify-between w-full items-center"
                     style="margin-right: 1rem"
                 >
                     <span class="flex-1"> {{ room.ort }} - {{ room.otium }} </span>
-                    <span class="inline-flex gap-3 items-baseline">
+                    <span v-if="!filterActive" class="inline-flex gap-3 items-baseline">
                         {{ room.einschreibungen.length }} Schüler:innen
                         <Button
                             :label="room.sindAnwesenheitenErfasst ? 'Fertig' : 'Ausstehend'"
@@ -152,6 +200,7 @@ function initMoveHere(terminId) {
                     :enrollments="room.einschreibungen"
                     :update-function="updateAttendanceCallback"
                     :block-id="blockId"
+                    :hide-move-here="filterActive"
                     may-edit-attendance
                     show-transfer
                     @initMove="(student) => initMove(student)"
