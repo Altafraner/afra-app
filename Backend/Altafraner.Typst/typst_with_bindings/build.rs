57ff0dc3574f02ce33707f9a43ff6e21c404ed3f
Johann Höpfner <mail@johann-hoepfner.de>
2026-03-19 17:31:00 +0100

    Move typst bindings to another repo

--- Backend/Altafraner.Typst/typst_with_bindings/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() -> () {
-    csbindgen::Builder::default()
-        .input_extern_file("src/lib.rs")
-        .csharp_dll_name("typst_with_bindings")
-        .generate_csharp_file("Typst.g.cs")
-        .unwrap();
-}
