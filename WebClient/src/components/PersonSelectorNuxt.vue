cab696704420d6bb21cd419359af2d05ec89580b
Richard Schlossarek <r.schlossarek@verein-der-altafraner.de>
2026-07-15 18:33:09 +0200

    Migrate otium schoolday management to Nuxt UI

--- WebClient/src/components/PersonSelectorNuxt.vue
+++ WebClient/src/components/PersonSelectorNuxt.vue
@@ -4,19 +4,17 @@ import { useOtiumStore } from '@/Otium/stores/otium.js';
 import { computed, ref } from 'vue';
 import { UserInfoMinimal } from '@/models/user/user';
 
-const model = defineModel<string | undefined>();
+const model = defineModel<string | string[] | undefined>();
 
 const settings = useOtiumStore();
 const loading = ref(true);
 
 const props = withDefaults(
     defineProps<{
-        multi?: boolean;
         hideRolle?: boolean;
         filter?: (student: UserInfoMinimal) => boolean;
     }>(),
     {
-        multi: false,
         hideRolle: false,
         filter: () => true,
     },
@@ -48,24 +46,10 @@ const personenMapped = computed(() => {
 </script>
 
 <template>
-    <USelectMenu v-model="model" :items="personenMapped" :loading="loading" value-key="id" />
+    <USelectMenu
+        v-model="model as any"
+        :items="personenMapped"
+        :loading="loading"
+        value-key="id"
+    />
 </template>
-
-<style scoped>
-.multiselect-wrap :deep(.p-multiselect-label-container) {
-    height: auto;
-}
-
-.multiselect-wrap :deep(.p-multiselect-label) {
-    display: flex;
-    flex-wrap: wrap;
-    white-space: normal;
-    gap: 0.25rem;
-    padding-top: 0.25rem;
-    padding-bottom: 0.25rem;
-}
-
-.multiselect-wrap :deep(.p-multiselect-token) {
-    margin-bottom: 0.25rem;
-}
-</style>
