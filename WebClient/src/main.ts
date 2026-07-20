90ae09e40683bf9ab2cd55e2660fe5061269334e
Richard Schlossarek <r.schlossarek@verein-der-altafraner.de>
2026-07-20 15:22:51 +0200

    convert routes to ts

--- WebClient/src/main.ts
+++ WebClient/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
+import router from './router/index';
 import ui from '@nuxt/ui/vue-plugin';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
