3172c3947b844fefc6d4ab8b469b42a9643a7e04
Johann Höpfner <mail@johann-hoepfner.de>
2026-07-25 21:24:45 +0000

    Add WantedEinschreibungen Input in Profundum Frontend

--- WebClient/src/Profundum/models/verwaltung.ts
+++ WebClient/src/Profundum/models/verwaltung.ts
@@ -22,6 +22,7 @@ export interface ProfundumInstanz {
     profundumInfo: ProfundumDefinition;
     slots: string[];
     maxEinschreibungen: null | number;
+    wantedEinschreibungen: null | number;
     numEinschreibungen: number;
     verantwortlicheIds: string[];
     verantwortlicheInfo: UserInfoMinimal[];
